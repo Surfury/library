08e27a539fa33b5697eb44d135d54efa317dbbc2
Surfury <68794693+Surfury@users.noreply.github.com>
2021-03-15 22:27:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,43 @@
 const eleID = ID => document.getElementById(ID);
-let library = [], id = 0;
+let library = [];
 window.addEventListener('load', () => {
   eleID('spinner').remove();
   eleID('cover').classList.add('hidden');
   eleID('cover').addEventListener('click', (e) => hideCover(e));
   document.documentElement.style.setProperty('--light-gray', '#aaaa');
 });
-/* update library in localStorage */
-const updateLS = () => {
-  localStorage.clear();
-  if(library.length>0) localStorage.setItem('library', JSON.stringify(library));
-}
 /* add a new book and render it */
 const addNewBook = (title, author, pages, isRead) => {
-  for (let i = library.length - 1; i >= 0; i--) {
-    if (library[i].title === this.title) {
-      alert('This book was already added to the library');
-      break;
-    }
-  }
   library.push({
     title: title,
     author: author,
     pages: pages,
     isRead: isRead
   });
-  eleID('books').innerHTML = `<div class='card'>
-  <button value='${id}'>x</button>
-  <p title='Title'>${title}</p>
+  updateBooks();
+}
+/* display books */
+const updateBooks = () => {
+  eleID('books').innerHTML = '';
+  library.forEach(E => {
+    eleID('books').innerHTML += `<div class='card'>
+  <button>x</button>
+  <p title='Title'>${E.title}</p>
       <hr>
-      <p title='Author'>${author}</p>
+      <p title='Author'>${E.author}</p>
       <hr>
-      <p title='Number of Pages'>${pages}</p>
+      <p title='Number of Pages'>${E.pages}</p>
       <hr>
       <label class='check'>Is Read?
-      <input type='checkbox' ${isRead} disabled>
+      <input type='checkbox' ${E.isRead} disabled>
       </label>
-  </div>` + eleID('books').innerHTML;
-
-  id++;
-
-  document.querySelectorAll('div.card > button').forEach(E => {
-    E.addEventListener('click', (e) => deleteBook(e.target.value));
+  </div>`;
   });
+  document.querySelectorAll('div.card > button').forEach(E => {
+    E.addEventListener('click', (e) => deleteBook(e.target));
+  });
+  localStorage.clear();
+  if (library.length > 0) localStorage.setItem('library', JSON.stringify(library));
 }
 class Book {
   constructor(title, author, pages, isRead) {
@@ -56,12 +51,12 @@ class Book {
   }
 }
 /* delete book if you click 'x' */
-const deleteBook = id => {
-  library.pop(id);
-  document.querySelector(`div.card > button[value='${id}']`).parentElement.classList.add('hidden');
-  /* wait to complete the animation */
-  setTimeout(() => document.querySelector(`div.card > button[value='${id}']`).parentElement.remove(), 490);
-  updateLS();
+function deleteBook(E) {
+  library = library.filter(Ele => Ele.title !== E.parentElement.firstElementChild.nextElementSibling.textContent);
+  /* wait to complete the transition */
+  E.parentElement.classList.add('hidden')
+  setTimeout(() => E.parentElement.remove(), 490);
+  updateBooks();
 }
 const numberOnly = e => {
   if (!e.key.match(/\d/)) {
@@ -97,7 +92,6 @@ eleID('addNewBook').addEventListener('click', () => {
   /* to hide the cover */
   eleID('cover').dispatchEvent(new Event('click'));
   eleID('addNewBook').disabled = 'disabled';
-  updateLS();
 });
 /* test the length of inputs */
 document.querySelectorAll('input[type="text"]').forEach(E => {
